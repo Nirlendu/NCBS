54efe7cd1770e6e587cb58ff731eb88ba7bc91be
Nirlendu <nirlendu@gmail.com>
2013-07-07 03:01:58 +0530

    Create preprocessing_config.m

---
+++ preprocessing_config.m
@@ -0,0 +1,13 @@
+inputdirec='C:\Users\Skullboy\Desktop\Archana_Lab\Data\Study1\';
+%sub='subject';
+%anatimg='C:\Users\Skullboy\Desktop\dataset\sM00223\sM00223_002.nii,1';
+%eval(subjects);
+sub='C:\Users\Skullboy\Desktop\Archana_Lab\Subject_lists\';
+%sub=subjectss;
+nslicess=27;
+tri=2;  				%tr
+so=[1:1:27];
+refslice=1;
+bb=[-78  -112   -112;78    76    85];
+prefix='w';
+smooth_fwhm = [8 8 8];
